22361949484a3818c4e1090fc006dd5abb7f3cd2
nabin <nkbhaie2017@gmail.com>
2022-08-06 10:39:57 -0600

    add a Chip Component

---
+++ src/Chips.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Chips = () => {
+    return (
+        <div>
+            <p>Many Bags Eaten!!!</p>
+            Go Back
+        </div>
+    )
+}
+
+export default Chips;
---
+++ src/VendingMachine.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import { NavLink } from 'react-router-dom';
+import Chips from './Chips';
+
+const VendingMachine = () => {
+    return (
+        <div>
+            {/* 3 navlinks */}  
+            <p>React Router, Links, and NavLinks</p>
+
+            <Chips />
+
+        </div>
+    )
+}
+
+export default VendingMachine;
